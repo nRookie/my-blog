90a46a5b00e57f12975c956bb21b9725ca7dc068
nRookie <naqing19950908@gmail.com>
2023-07-01 11:00:38 +0900

    implement the basic crud

--- my-blog/src/components/Post.js
+++ my-blog/src/components/Post.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 function Post() {
   const { id } = useParams();
   const [post, setPost] = useState({});
+  const navigate = useNavigate();
 
   useEffect(() => {
     axios.get(`http://localhost:3000/posts/${id}`)
@@ -16,11 +17,29 @@ function Post() {
       });
   }, [id]);
 
+
+
+  const deletePost = async () => {
+    const confirmDelete = window.confirm('Are you sure you want to delete this post?');
+
+    if (confirmDelete) {
+      try {
+        await axios.delete(`http://localhost:3000/posts/${id}`);
+        // After deleting, redirect to home page
+        // You might need to use useHistory from react-router-dom for redirection
+        navigate('/');
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
+
   console.log(post)
   return (
     <div>
       <h2>{post.title}</h2>
       <p>{post.content}</p>
+      <button onClick={deletePost}>Delete Post</button>
     </div>
   );
 }

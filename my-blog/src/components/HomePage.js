a1e051c993ea77891846472f617463275c9a0710
nRookie <naqing19950908@gmail.com>
2023-07-01 11:49:12 +0900

    add css and implement basic functions

---
+++ my-blog/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function HomePage() {
+    return (
+        <div className="home-page">
+            <p>Step by step, day by day.</p>
+        </div>
+    );
+}
+
+export default HomePage;
+

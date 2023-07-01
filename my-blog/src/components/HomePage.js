688d44d74fbce08ddbbd9d8f38d2a7ec331e5f56
nRookie <naqing19950908@gmail.com>
2023-07-01 14:53:20 +0900

    add some picture in home page

--- my-blog/src/components/HomePage.js
+++ my-blog/src/components/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
-
+import homeImage from '../resources/beach.jpeg';
+import "./HomePage.css"
 function HomePage() {
     return (
         <div className="home-page">
-            <p>Step by step, day by day.</p>
+            <img src={homeImage} alt="Home" className="homepageImage" />
+            <h1 className="homepageTitle">Step by step, day by day</h1>
         </div>
     );
 }

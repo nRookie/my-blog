b591128c35ef343544b4442e072419ac10a8d83f
nRookie <naqing19950908@gmail.com>
2023-07-04 20:09:19 +0900

    show it seperately

--- my-blog/src/components/vocabulary/Vocabulary.js
+++ my-blog/src/components/vocabulary/Vocabulary.js
@@ -1,61 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import axios from "axios";
-import serverAddress from "../../config";
-
-// Material-UI imports
+// Vocabulary.js
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
-import { makeStyles } from '@material-ui/core/styles';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Paper from '@material-ui/core/Paper';
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    padding: theme.spacing(2),
-    margin: theme.spacing(2),
-    textAlign: 'left',
-    color: theme.palette.text.secondary,
-  },
-}));
 
 const Vocabulary = () => {
-    const [vocabulary, setVocabulary] =  useState(null);
-    const classes = useStyles();
-
-    // Display data when it's available
-    useEffect(() => {
-        axios.get(`${serverAddress}/vocabulary_day/`)
-            .then(res => {
-                setVocabulary(res.data);
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    }, []);
-
     return (
         <Container>
             <Box my={4}>
                 <Typography variant="h3" align="center">Vocabulary Camp</Typography>
-                {/* If you want to display the vocabData on the page */}
-                {vocabulary ? (
-                    vocabulary.map((vocabItem, index) => (
-                        <Link to={`/vocabulary/day/${vocabItem.day}`} key={index}>
-                            <Paper className={classes.paper}>
-                                <Typography variant="h6">Day: {vocabItem.day}</Typography>
-                                <Typography variant="body1">Description: {vocabItem.description}</Typography>
-                            </Paper>
-                        </Link>
-                    ))
-                ) : (
-                    <Box display="flex" justifyContent="center">
-                        <CircularProgress />
-                    </Box>
-                )}
+                <Outlet /> {/* This will render either VocabularyDayList or VocabularyDay */}
             </Box>
-            <Outlet /> 
         </Container>
     );
 }
--- my-blog/src/components/vocabulary/VocabularyDayList.js
+++ my-blog/src/components/vocabulary/VocabularyDayList.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+// VocabularyDayList.js
+import React, { useEffect, useState } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
+import axios from 'axios';
+import serverAddress from "../../config";
 
 // Material-UI imports
 import List from '@material-ui/core/List';
@@ -9,20 +11,37 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 const VocabularyDayList = () => {
-    // Get the vocabData from the Redux store instead of local state
-    const vocabData = useSelector(state => state.vocabulary.vocabData);
-    
+    const [vocabulary, setVocabulary] = useState(null);
+
+    useEffect(() => {
+        // Fetch data when the component is mounted
+        axios.get(`${serverAddress}/vocabulary_day/`)
+            .then(res => {
+                setVocabulary(res.data);
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }, []);
+
+    if (!vocabulary) {
+        return (
+            <CircularProgress />
+        );
+    }
+
     return (
         <List>
-            {vocabData.map((vocab, index) => (
+            {vocabulary.map((vocabItem, index) => (
                 <ListItem key={index}>
                     <Card style={{ width: '100%' }}>
                         <CardContent>
-                            <Link component={RouterLink} to={`day/${vocab.day}`}>
+                            <Link component={RouterLink} to={`/vocabulary/day/${vocabItem.day}`}>
                                 <Typography variant="h5">
-                                    Day {vocab.day}
+                                    Day {vocabItem.day}
                                 </Typography>
                             </Link>
                         </CardContent>

302e88a80a22b5fb4e19b07d4d8875b352d3be78
nRookie <naqing19950908@gmail.com>
2023-07-04 20:04:11 +0900

    makes the vocabulary page more beautiful

--- my-blog/src/components/vocabulary/VocabularyDayList.js
+++ my-blog/src/components/vocabulary/VocabularyDayList.js
@@ -1,22 +1,35 @@
-// VocabularyDayList.js
 import React from 'react';
 import {useSelector} from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 
-  
+// Material-UI imports
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
 const VocabularyDayList = () => {
     // Get the vocabData from the Redux store instead of local state
     const vocabData = useSelector(state => state.vocabulary.vocabData);
+    
     return (
-        <ul>
+        <List>
             {vocabData.map((vocab, index) => (
-                <li key={index}>
-                    <Link to={`day/${vocab.day}`}>
-                        Day {vocab.day}
-                    </Link>
-                </li>
+                <ListItem key={index}>
+                    <Card style={{ width: '100%' }}>
+                        <CardContent>
+                            <Link component={RouterLink} to={`day/${vocab.day}`}>
+                                <Typography variant="h5">
+                                    Day {vocab.day}
+                                </Typography>
+                            </Link>
+                        </CardContent>
+                    </Card>
+                </ListItem>
             ))}
-        </ul>
+        </List>
     );
 }
 

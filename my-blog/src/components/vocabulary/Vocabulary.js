801e89cd3ff59396cb8a942d25565b430a2d5ce0
nRookie <naqing19950908@gmail.com>
2023-07-04 18:36:51 +0900

    makes the page more beautiful with material ui

--- my-blog/src/components/vocabulary/Vocabulary.js
+++ my-blog/src/components/vocabulary/Vocabulary.js
@@ -3,8 +3,26 @@ import { Outlet } from 'react-router-dom';
 import axios from "axios";
 import serverAddress from "../../config";
 
+// Material-UI imports
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Paper from '@material-ui/core/Paper';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    margin: theme.spacing(2),
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 const Vocabulary = () => {
     const [vocabulary, setVocabulary] =  useState(null);
+    const classes = useStyles();
 
     // Display data when it's available
     useEffect(() => {
@@ -18,17 +36,25 @@ const Vocabulary = () => {
     }, []);
 
     return (
-        <div>
-            <h1>Vocabulary Camp</h1>
-            {/* If you want to display the vocabData on the page */}
-            {vocabulary ? (
-                // Replace this with how you want to render your data
-                <div>{JSON.stringify(vocabulary)}</div>
-            ) : (
-                <div>Loading...</div>
-            )}
-            <Outlet />
-        </div>
+        <Container>
+            <Box my={4}>
+                <Typography variant="h3" align="center">Vocabulary Camp</Typography>
+                {/* If you want to display the vocabData on the page */}
+                {vocabulary ? (
+                    vocabulary.map((vocabItem, index) => (
+                        <Paper key={index} className={classes.paper}>
+                            <Typography variant="h6">Day: {vocabItem.day}</Typography>
+                            <Typography variant="body1">Description: {vocabItem.description}</Typography>
+                        </Paper>
+                    ))
+                ) : (
+                    <Box display="flex" justifyContent="center">
+                        <CircularProgress />
+                    </Box>
+                )}
+                <Outlet />
+            </Box>
+        </Container>
     );
 }
 

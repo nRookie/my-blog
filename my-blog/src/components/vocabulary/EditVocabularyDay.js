719fb313463359b3b108df4e422fc447ca679577
nRookie <naqing19950908@gmail.com>
2023-07-11 12:08:40 +0900

    implement the simple retrieve method, do not implement the update method yet

--- my-blog/src/components/vocabulary/EditVocabularyDay.js
+++ my-blog/src/components/vocabulary/EditVocabularyDay.js
@@ -2,16 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import serverAddress from "../../config"; 
 import axios from 'axios';
-
 const EditVocabularyDay = () => {
-    const [vocabularyDay, setVocabularyDay] = useState({});
+    const [vocabularyDay, setVocabularyDay] = useState({day: '', description: ''});
     const { day } = useParams();
     // Fetch the vocabulary day when the component mounts
     useEffect(() => {
         axios.get(`${serverAddress}/vocabulary_day/${day}`)
             .then(response => setVocabularyDay(response.data))
             .catch(error => console.error(`There was an error retrieving the vocabulary day: ${error}`));
-    }, []);
+    }, [day]);
 
     // Handle the changes of the form
     const handleChange = (event) => {
@@ -28,15 +27,16 @@ const EditVocabularyDay = () => {
 
     return (
         <form onSubmit={handleSubmit}>
-            <label htmlFor="word">Day</label>
-            <input id="word" name="word" value={vocabularyDay.Day || ''} onChange={handleChange} />
+            <label htmlFor="day">Day</label>
+            <input id="day" name="day" value={vocabularyDay.day || ''} onChange={handleChange} />
 
-            <label htmlFor="definition">Description</label>
-            <input id="definition" name="Description" value={vocabularyDay.definition || ''} onChange={handleChange} />
+            <label htmlFor="description">Description</label>
+            <input id="description" name="description" value={vocabularyDay.description || ''} onChange={handleChange} />
 
             <button type="submit">Update Vocabulary Day</button>
         </form>
     )
 }
+ 
 
 export default EditVocabularyDay;

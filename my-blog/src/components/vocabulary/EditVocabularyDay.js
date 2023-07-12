9f6e914b1ecc0c4124c89551cac5ccd932bfabb8
nRookie <naqing19950908@gmail.com>
2023-07-12 09:19:55 +0900

    makes the page more beautiful

--- my-blog/src/components/vocabulary/EditVocabularyDay.js
+++ my-blog/src/components/vocabulary/EditVocabularyDay.js
@@ -1,10 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import serverAddress from "../../config"; 
 import axios from 'axios';
+import serverAddress from "../../config";
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    margin: theme.spacing(1),
+  },
+  textField: {
+    marginBottom: theme.spacing(2),
+  },
+  button: {
+    marginTop: theme.spacing(1),
+  },
+}));
+
 const EditVocabularyDay = () => {
+    const classes = useStyles();
     const [vocabularyDay, setVocabularyDay] = useState({day: '', description: ''});
     const { day } = useParams();
+    const navigate = useNavigate();
+
     // Fetch the vocabulary day when the component mounts
     useEffect(() => {
         axios.get(`${serverAddress}/vocabulary_day/${day}`)
@@ -25,22 +48,39 @@ const EditVocabularyDay = () => {
                 'Authorization': `Bearer ${localStorage.getItem('token')}`
             }
         })
-            .then(() => console.log('Vocabulary Day Updated'))
+            .then(() => {
+              console.log('Vocabulary Day Updated');
+              navigate('/vocabulary');
+            })
             .catch(error => console.error(`There was an error updating the vocabulary day: ${error}`));
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="day">Day</label>
-            <input id="day" name="day" value={vocabularyDay.day || ''} onChange={handleChange} />
-
-            <label htmlFor="description">Description</label>
-            <input id="description" name="description" value={vocabularyDay.description || ''} onChange={handleChange} />
-
-            <button type="submit">Update Vocabulary Day</button>
+      <Container maxWidth="sm">
+        <Typography variant="h4" align="center" gutterBottom>Edit Vocabulary Day</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <TextField 
+            className={classes.textField}
+            id="day"
+            name="day"
+            label="Day"
+            value={vocabularyDay.day || ''}
+            onChange={handleChange} 
+            required 
+          />
+          <TextField 
+            className={classes.textField}
+            id="description"
+            name="description"
+            label="Description"
+            value={vocabularyDay.description || ''}
+            onChange={handleChange} 
+            required 
+          />
+          <Button className={classes.button} variant="contained" color="primary" type="submit">Update Vocabulary Day</Button>
         </form>
+      </Container>
     )
 }
- 
 
 export default EditVocabularyDay;
